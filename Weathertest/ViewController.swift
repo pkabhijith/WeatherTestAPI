0e564c6672296be7025cff06a17fbcb987816e3d
Abhijith Kumar <abhijith.prasanna92@gmail.com>
2018-02-07 22:27:02 -0500

    Created a successful JSON parser. It can parse through the weather data and fetch the information we need. Printed an output to the console.

--- Weathertest/ViewController.swift
+++ Weathertest/ViewController.swift
@@ -12,27 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(123)
-        
-        //let basepath = "http://api.apixu.com/v1/forecast.json?key=2f076e71ad814f6bb16235617180602&q=Greenbelt&days=5"
-        
-        //guard let url = URL(string: basepath) else { return}
-        
-       // URLSession.shared.dataTask(with: url)  { (data:Data?, response:URLResponse?, error:Error?) in
-            
-         //   guard let sata = data else {return}
-            
-           // if let dataAsString = String(data:sata, encoding: .utf8) {
-            //    print(dataAsString)
-           // }
-            
-       // }
-       // .resume()
         
         Weather.forecast { (results:[Weather]) in
             for result in results {
                 print("\(result)\n\n")
-                print(123)
             }
     }
     }
--- Weathertest/Weather.swift
+++ Weathertest/Weather.swift
@@ -8,96 +8,100 @@
 
 import Foundation
 
-
 struct Weather  {
     
     let summary : String
     let icon: String
-    let temp_f: Double
+    let maxTempOfDay: Double
     
-    enum SerialError:Error {
-        case missing(String)
-        case invalid(String, Any)
+    init?(json:[String:Any]) {
+        
+       //guard let astro = json["astro"] as? [String:Any],
+            //let date = json["date"] as? String,
+            //let date_epoch = json["date_epoch"] as? Int,
+      guard let day = json["day"] as? [String:Any],
+            let condition = day["condition"] as? [String:Any],
+            let summary = condition["text"],
+            let icon = condition["icon"],
+            let maxTempOfDay = day["maxtemp_f"]
+        else {
+            return nil
+        }
+        self.summary=summary as! String
+        self.icon = icon as! String
+        self.maxTempOfDay=maxTempOfDay as! Double
     }
     
-    init(json:[String:Any]) throws {
-        guard let summary = json["text"] as? String else { throw SerialError.missing("Summary text is missing")}
+    struct Location {
         
-        guard let icon = json["icon"] as? String else { throw SerialError.missing("Icon is missing")}
+        let city:String
+        let region: String
+        let localTime: String
+    
+        init?(json:[String:Any]) {
+            
+            guard let location = json["location"] as? [String:Any],
+                  let city = location["name"],
+                  let region = location["region"],
+                  let localTime=location["localtime"]
+                else {
+                    return nil
+            }
+            self.city=city as! String
+            self.region=region as! String
+            self.localTime = localTime as! String
+        }
+    }
+    
+    struct CurrentWeather {
         
-        guard let temp_f = json["temp_f"] as? Double else { throw SerialError.missing("Temperature is missing")}
-        
-        self.summary=summary
-        self.icon = icon
-        self.temp_f=temp_f
+        let temperatureCelsius: Double
+        let temperatureFarenheit: Double
+        let summary: String
+        let icon: String
+        let feelslikeCelsius:Double
+        let feelslikeFarenheit:Double
         
     }
+    
     
     static let basepath = "http://api.apixu.com/v1/forecast.json?key=2f076e71ad814f6bb16235617180602&q=Greenbelt&days=5"
-    
     static func forecast (completion: @escaping ([Weather]) -> ()) {
-        
-        guard let request = URL(string: basepath) else { return}
-        
-        //URLSession.shared.dataTask(with: request)  { (data:Data?, response:URLResponse?, error:Error?) in
-            
-            //guard let sata = data else {return}
-            
-            //if let dataAsString = String(data:sata, encoding: .utf8) {
-              //  print(dataAsString)
-        
+    guard let request = URL(string: basepath) else { return}
 
         
-        URLSession.shared.dataTask(with: request) { (data:Data?, response:URLResponse?, error:Error?) in
-            
-           // var forcastArray:[Weather] = []
-            
-            //guard let sata = data else {return}
-            //let dataAsString = String(describing: sata).utf8
-            //print(dataAsString)
-            
-            if let data = data {
-                do {
-                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] {
-                        //print(json)
-                        if let dailyForecast = json["forecast"] as?  [String:Any] {
-                            print (dailyForecast)
-                            if let forecastDay = dailyForecast["forecastday"] as? [[String:Any]] {
-                                //print (forecastDay)
-                                //if let day = forecastDay[["condition"]] as? [<String>] {
-                                    //print (day)
-                                    //for dataPoint in day {
-                                    //if let weatherObject = try? Weather(json:dataPoint) {
-                                      //  forcastArray.append(weatherObject)
-                                                
-                                        
-                                      //  }
-                                        
-                                        
-                                  //  }
-                                //}
-                            }
+    let task = URLSession.shared.dataTask(with: request) { (data,response,error) -> Void in
+        var forecastArray : [Weather] = []
+        if let data = data {
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String:Any] {
+                    //print(json)
+                    if let dailyForecast = json["forecast"] as?  [String:Any] {
+                        //print (dailyForecast)
+                        if let forecastday = dailyForecast["forecastday"] as? [[String:Any]] {
+                            for case let everyday in forecastday  {
+                            //print (everyday)
+                                if let eachResult = Weather(json:everyday) {
+                                    forecastArray.append(eachResult)
+                                }
                         }
-                        
                     }
-                }
-                catch let error as URLError {
                     
-                    print(error.localizedDescription)
-                }
-                catch {
-                    print(error)
-                }
-                
-                //completion(forcastArray)
+                    }}
+
+            } catch let error as Error {
+                print(error.localizedDescription)
+            }
+        } else if let error = error {
+            print(error.localizedDescription)
         }
-      
-    }
-        .resume()
-
+        completion(forecastArray)
+        //print (forecastArray)
+        }
+       
+task.resume()
 }
 }
-
 
 
 
